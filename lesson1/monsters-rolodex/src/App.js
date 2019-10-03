ac3e237c573d32af348585b7ea9620d232104f15
Jordan <iraborjordan1@gmail.com>
2019-10-03 18:48:55 +0100

    destructured monsters and searchField and rerenderd based on new values

--- lesson1/monsters-rolodex/src/App.js
+++ lesson1/monsters-rolodex/src/App.js
@@ -20,10 +20,14 @@ export default class App extends React.Component {
   }
 
   render() {
+
+    const {monsters, searchField} = this.state;
+    const filteredMonsters =monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+
     return (
       <div className="App">
       <input type="search" placeholder="search for monsters" onChange={e => this.setState({searchField: e.target.value})} />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
