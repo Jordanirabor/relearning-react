4fae4eb8f1f2d90ff0433ca8e08fbaff8c847749
Jordan <iraborjordan1@gmail.com>
2019-10-29 23:59:12 +0100

    included the rough draft for the home component

---
+++ lesson5/src/homepage.component.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default const Homepage = () => (
+<div className='homepage'>
+<div className='directory-menu'>
+<div className='menu-item'>
+<div className='content'>
+<h1 className ='title'>HATS </h1>
+<span className='subtitle'> SHOP NOW</span>
+</div>
+</div>
+
+<div className='menu-item'>
+<div className='content'>
+<h1 className ='title'> JACKETS </h1>
+<span className='subtitle'> SHOP NOW</span>
+
+</div>
+</div>
+
+<div className='menu-item'>
+<div className='content'>
+<h1 className ='title'>WOMENS</h1>
+<span className='subtitle'> SHOP NOW</span>
+
+</div>
+</div>
+
+<div className='menu-item'>
+<div className='content'>
+<h1 className ='title'>SNEAKRS</h1>
+<span className='subtitle'> SHOP NOW</span>
+
+</div>
+</div>
+</div>
+</div>
+)
