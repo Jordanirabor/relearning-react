d329ce3902c9b301549d88f30c4b7c68f711a037
Jordan <iraborjordan1@gmail.com>
2019-11-08 01:50:43 +0100

    created a new component 'CollectionPreview' along with its styles.

---
+++ lesson5/src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const CollectionPreview = ({ title, item }) => (
+    <div className="collection-preview">
+        <h2 className="title">
+            {title}
+        </h2>
+        <p> item</p>
+    </div>
+)
+
+export default CollectionPreview
