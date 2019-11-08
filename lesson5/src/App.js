616f9b72b240bfe61111fcd94294363b196f28c8
Jordan <iraborjordan1@gmail.com>
2019-11-08 01:47:51 +0100

    created a new page 'ShopPage' along with its styles and data.

--- lesson5/src/App.js
+++ lesson5/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Homepage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 import { Switch, Route } from 'react-router-dom';
 
 function Hats() {
@@ -14,8 +15,8 @@ function App() {
   return (
     <div>
       <Switch>
-      <Route exact path='/' component={Homepage} />
-      <Route exact path='/shop/hats' component={Hats} />
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
