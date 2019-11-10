a5deacb0eaeeaaec5fad7c9ce6da49e9509ff16f
Jordan <iraborjordan1@gmail.com>
2019-11-10 05:41:14 +0100

    included styles for header and adjusted JSX in the header's component to display correctly

--- lesson5/src/components/header/header.component.jsx
+++ lesson5/src/components/header/header.component.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { ReactComponent as Logo } from '../../assets/logo/crown.svg'
+import './header.styles.scss'
 
 const Header = () => (
     <div className="header">
-        <div className="header">
             <Link className="logo-container" to="/">
                 <Logo className="logo" />
             </Link>
-        </div>
 
         <div className="options">
             <Link className="option" to="/shop">
