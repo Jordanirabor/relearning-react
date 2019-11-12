d4cb09447616a5ec8851ca311a355116a2bfccfb
Jordan <iraborjordan1@gmail.com>
2019-11-12 23:10:11 +0100

    added 'SIGN IN' as a Link in the header component

--- lesson5/src/components/header/header.component.jsx
+++ lesson5/src/components/header/header.component.jsx
@@ -5,9 +5,9 @@ import './header.styles.scss'
 
 const Header = () => (
     <div className="header">
-            <Link className="logo-container" to="/">
-                <Logo className="logo" />
-            </Link>
+        <Link className="logo-container" to="/">
+            <Logo className="logo" />
+        </Link>
 
         <div className="options">
             <Link className="option" to="/shop">
@@ -15,6 +15,9 @@ const Header = () => (
         </Link>
             <Link className="option" to="/contact">
                 CONTACT
+        </Link>
+            <Link className="option" to="/signin">
+                SIGN IN
         </Link>
         </div>
     </div>
