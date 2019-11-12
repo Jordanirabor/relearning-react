19120f91491c9e44842a0a00c8fefb0c8407ca7b
Jordan <iraborjordan1@gmail.com>
2019-11-12 23:06:23 +0100

    created the folder and file structure for the sign in and sign up page (components)

---
+++ lesson5/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './sign-in-and-sign-up.styles.scss'
+
+
+const SIgnInAndSignOutPage = () => (
+    <div className="sign-in-and-sign-up"> SIGN IN </div>
+)
+
+export default SIgnInAndSignOutPage
