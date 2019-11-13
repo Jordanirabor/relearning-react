0ac7f2b90486d4f10b3bcf8770eff20e021eb0d8
Jordan <iraborjordan1@gmail.com>
2019-11-13 23:02:37 +0100

    converted handler functions to fat arrow functions to bind this to the correct context => the class component

--- lesson5/src/components/sign-in/sign-in.component.jsx
+++ lesson5/src/components/sign-in/sign-in.component.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import './sign-in.styles.scss'
 
 export default class SignIn extends React.Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
 
         this.state = {
             email: '',
@@ -11,13 +11,14 @@ export default class SignIn extends React.Component {
         }
     }
 
-    handleChange(e) {
+    handleChange = e => {
         const { name, value } = e.target
         this.setState({ [name]: value })
     }
 
-    handleSubmit() {
-        this.setState({ name: '', password: '' })
+    handleSubmit = e => {
+        e.preventDefault();
+        this.setState({ email: '', password: '' })
     }
 
     render() {
@@ -27,11 +28,13 @@ export default class SignIn extends React.Component {
                 <h2>I already have an account </h2>
                 <span>Sign in with your email and password </span>
 
-                <input type="email" name="email" placeholder="Email" onChange={this.handleChange} />
-                <label>Email</label>
-                <input type="password" name="password" placeholder="Password" onChange={this.handleChange} />
-                <label>Email</label>
-                <input type="submit" placeholder="Submir" onChange={this.handleSubmit} />
+                <form onSubmit={this.handleSubmit}>
+                    <input type="email" name="email" placeholder="Email" value={this.state.email} required onChange={this.handleChange} />
+                    <label>Email</label>
+                    <input type="password" name="password" placeholder="Password" value={this.state.password} required onChange={this.handleChange} />
+                    <label>Email</label>
+                    <input type="submit" placeholder="Submit" />
+                </form>
             </div>
         )
 
