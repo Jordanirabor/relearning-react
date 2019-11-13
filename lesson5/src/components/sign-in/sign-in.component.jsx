9e68af7d71e9296167eb80d23095229d2be9c568
Jordan <iraborjordan1@gmail.com>
2019-11-13 22:46:38 +0100

    added the JSX UI definition for the sign in component

--- lesson5/src/components/sign-in/sign-in.component.jsx
+++ lesson5/src/components/sign-in/sign-in.component.jsx
@@ -23,6 +23,11 @@ export default class SignIn extends React.Component {
             <h2>I already have an account </h2>
             <span>Sign in with your email and password </span>
 
+            <input type="email" name="email" placeholder="Email" onChange={this.handleChange} />
+            <label>Email</label>
+            <input type="password" name="password" placeholder="Password" onChange={this.handleChange} />
+            <label>Email</label>
+            <input type="submit" placeholder="Submir" onChange={this.handleSubmit} />
         </div>
     }
 }
