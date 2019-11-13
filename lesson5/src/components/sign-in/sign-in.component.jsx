158be69fca56f30a216c85a7301b4a97bdb47912
Jordan <iraborjordan1@gmail.com>
2019-11-13 22:52:18 +0100

    included code for 'handleChange' and 'handleSubmit' functions

--- lesson5/src/components/sign-in/sign-in.component.jsx
+++ lesson5/src/components/sign-in/sign-in.component.jsx
@@ -1,34 +1,40 @@
 import React from 'react'
+import './sign-in.styles.scss'
 
 export default class SignIn extends React.Component {
     constructor() {
         super()
 
         this.state = {
-            name: '',
+            email: '',
             password: ''
         }
     }
 
-    handleChange() {
-
+    handleChange(e) {
+        const { name, value } = e.target
+        this.setState({ [name]: value })
     }
 
     handleSubmit() {
-
+        this.setState({ name: '', password: '' })
     }
 
     render() {
-        <div class="sign-in">
-            <h2>I already have an account </h2>
-            <span>Sign in with your email and password </span>
 
-            <input type="email" name="email" placeholder="Email" onChange={this.handleChange} />
-            <label>Email</label>
-            <input type="password" name="password" placeholder="Password" onChange={this.handleChange} />
-            <label>Email</label>
-            <input type="submit" placeholder="Submir" onChange={this.handleSubmit} />
-        </div>
+        return (
+            <div class="sign-in">
+                <h2>I already have an account </h2>
+                <span>Sign in with your email and password </span>
+
+                <input type="email" name="email" placeholder="Email" onChange={this.handleChange} />
+                <label>Email</label>
+                <input type="password" name="password" placeholder="Password" onChange={this.handleChange} />
+                <label>Email</label>
+                <input type="submit" placeholder="Submir" onChange={this.handleSubmit} />
+            </div>
+        )
+
     }
 }
 
--- lesson5/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ lesson5/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './sign-in-and-sign-up.styles.scss'
+import SignIn from '../../components/sign-in/sign-in.component'
 
 
 const SIgnInAndSignOutPage = () => (
-    <div className="sign-in-and-sign-up"> SIGN IN </div>
+    <div className="sign-in-and-sign-up">
+        <SignIn />
+    </div>
 )
 
 export default SIgnInAndSignOutPage
