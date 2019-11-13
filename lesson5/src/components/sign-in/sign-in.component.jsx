b1aecb6e91b04084749caa390a754c874e5cfaef
Jordan <iraborjordan1@gmail.com>
2019-11-13 22:21:56 +0100

    built out the 'Sign In' component's folder and file structure

---
+++ lesson5/src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default class SignIn extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            name: '',
+            password: ''
+        }
+    }
+
+    handleChange() {
+
+    }
+
+    handleSubmit() {
+
+    }
+
+    render() {
+        <div class="sign-in">
+            <h2>I already have an account </h2>
+            <span>Sign in with your email and password </span>
+
+        </div>
+    }
+}
+
+
