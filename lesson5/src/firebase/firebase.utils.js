6f2fe0fc4519696eb5932c154938a29723315f7f
Jordan <iraborjordan1@gmail.com>
2019-12-22 22:56:18 +0100

    added try - catch block to catch errors in the creation of a new user document

--- lesson5/src/firebase/firebase.utils.js
+++ lesson5/src/firebase/firebase.utils.js
@@ -30,7 +30,20 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!snapShot.exists) {
         const { displayName, email } = userAuth;
         const createdAt = new Date()
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (err) {
+            console.log('error creating user', err.message)
+        }
     }
+
+    return userRef
 }
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider)
