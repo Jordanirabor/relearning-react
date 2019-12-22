e4b93fbc4d9824d888983aeaf0323e813bb5847d
Jordan <iraborjordan1@gmail.com>
2019-12-22 22:53:28 +0100

    created a createUserProfileDocument() method to create a new user record in firestore

--- lesson5/src/firebase/firebase.utils.js
+++ lesson5/src/firebase/firebase.utils.js
@@ -20,6 +20,19 @@ export const firestore = firebase.firestore();
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ "prompt": "select_account" })
 
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date()
+    }
+}
+
 export const signInWithGoogle = () => auth.signInWithPopup(provider)
 
 export default firebase;
