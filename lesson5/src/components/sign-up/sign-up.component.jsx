e3f4eb7e9590fef9948fdc1e454b18009d907f70
Jordan <iraborjordan1@gmail.com>
2019-12-26 22:37:42 +0100

    created the SignUp component's folder struture and imported its dependencies

---
+++ lesson5/src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils'
+import './sign-up.styles.scss'
+
+class SignUp extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            displayName: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
+    }
+}
